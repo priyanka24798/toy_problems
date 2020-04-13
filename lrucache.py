b422e64d6ecbf648e3c0169f19b2ca348d344f1c
priyanka24798 <kotupriyanka@msitprogram.net>
2020-04-13 22:35:18 +0530

    add constructor for LRU

---
+++ lrucache.py
@@ -0,0 +1,14 @@
+class lru_implementation:
+    
+    
+    def __init__(self, size):
+        pass
+
+    def put(self, key):
+        pass
+
+    def get(self, key, value):
+        pass
+
+    def get_cache(self):
+        pass
---
+++ testlru.py
@@ -0,0 +1,17 @@
+from lrucache import *
+
+def test_cases(self):
+
+ 
+
+    if __name__ == "__main__":
+        pass
+
+    
+
+
+
+
+
+
+
