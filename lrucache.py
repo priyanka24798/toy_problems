eaeebd0ed47df09ffd041b3850e54ed4aed47ff3
priyanka24798 <kotupriyanka@msitprogram.net>
2020-04-14 00:23:20 +0530

    Assignment 1 - lru

--- lrucache.py
+++ lrucache.py
@@ -1,14 +1,44 @@
-class lru_implementation:
+class lrucache:
     
     
-    def __init__(self, size):
-        pass
+    def __init__(self, length):
+
+        self.length = length
+        self.lru = []
+        self.lrudict = {}
+       
 
     def put(self, key):
-        pass
+        if (len(self.lru) < self.length):
 
-    def get(self, key, value):
-        pass
+             if key in self.lru:
+                 self.lru.remove(key)
+                 self.lru.append(key)
+                 value = str(key)+ " is from IIIT hyderabad"
+                 self.lrudict[key] = value
+             else:
+                self.lru.append(key)
+                value = str(key)+ " is from IIIT hyderabad"
+                self.lrudict[key] = value
+        else:
+             
+             r = self.lru.pop(0)
+             del self.lrudict[r]
+             self.lru.append(key)
+             value = str(key)+ " is from IIIT hyderabad"
+             self.lrudict[key] = value
+
+
+    def get(self, key, value = None):
+        
+        if key in self.lru:
+            return self.lrudict[key]
+        
+        else:
+            return value
 
     def get_cache(self):
-        pass
+        f = []
+        for key in reversed(self.lru):
+            f.append(key + " : " + self.lrudict[key])
+        return f
--- testlru.py
+++ testlru.py
@@ -1,11 +1,33 @@
-from lrucache import *
+from lrucache import lrucache
 
-def test_cases(self):
+class unit_test:
+    
+
+    def __init__(self):
+        pass
+
+    def test_cases(self):
+        a = lrucache(4)
+        a.put("priya")
+        a.put("hari")
+        a.put("abhi")
+        a.put("sunny")
+        assert a.get("priya") == "priya is from IIIT hyderabad", "case 1 failed"
+        assert a.get("abhi") == "abhi is from IIIT hyderabad", "case 2 failed"
+        assert a.get("hari") == "hari is from IIIT hyderabad", "case 3 failed"
+        assert a.get("sunny") == "sunny is from IIIT hyderabad", "case 4 failed"
+        a.put("priya") == None, "testcase failed"
+        print("All passed..!")
+       
+       #to print the cache items:
+        l = a.get_cache()
+        print(l)
+
+if __name__ == '__main__':
+    a = unit_test()
+    a.test_cases()
 
  
-
-    if __name__ == "__main__":
-        pass
 
     
 
